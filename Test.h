ca8b4d93c5c21e07d5244228a0ecceb713df5f5a
Mladen Tusup <Mladen@Mladens-MacBook-Pro.local>
2015-10-27 19:48:20 +0100

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitProba
+//
+//  Created by Mladen Tusup on 10/27/15.
+//  Copyright © 2015 Mladen Tusup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitProba
+//
+//  Created by Mladen Tusup on 10/27/15.
+//  Copyright © 2015 Mladen Tusup. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
